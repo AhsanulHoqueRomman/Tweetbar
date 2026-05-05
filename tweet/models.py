226e66e909b2fb83d3a650dcc82f13aecb41e28e
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-05-06 04:11:17 +0600

    integrating prodile model

--- tweet/models.py
+++ tweet/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to='profile_photos/',blank=True,null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
     text = models.TextField(max_length= 300)
